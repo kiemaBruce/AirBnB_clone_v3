e6a43442d24ec57114f84145b39aebeeaef33448
kiemaBruce <brucekiema292@gmail.com>
2024-09-24 17:20:52 +0300

    Include the /api/v1/stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,27 @@ from flask import jsonify
 def status():
     """Returns the API status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats_by_type():
+    """Retrieves the number of each objects by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+
+    stats_dict = {
+                    "amenities": Amenity,
+                    "cities": City,
+                    "places": Place,
+                    "reviews": Review,
+                    "states": State,
+                    "users": User
+                    }
+    for key, value in stats_dict.items():
+        stats_dict[key] = storage.count(value)
+    return jsonify(stats_dict)
